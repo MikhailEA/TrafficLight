2e987d166ac59c1a7a178e3427648f974cd83059
MikhailEA <michev.work@gmail.com>
2021-09-29 18:39:14 +0300

    Продолжение , добавление логики

--- app/src/main/java/com/android/trafficlight/MainActivity.java
+++ app/src/main/java/com/android/trafficlight/MainActivity.java
@@ -3,12 +3,20 @@ package com.android.trafficlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout b_1, b_2, b_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b_1 = findViewById(R.id.bulb_1);
+    }
+
+    public void onClickStart(View view) {
+
     }
 }
