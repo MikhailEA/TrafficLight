fa6bbf63b3e2037b3810996059e902f1d315a50f
MikhailEA <michev.work@gmail.com>
2021-10-01 18:22:27 +0300

    Редактирование активити, для запуска потоков и счетчика

--- app/src/main/java/com/android/trafficlight/MainActivity.java
+++ app/src/main/java/com/android/trafficlight/MainActivity.java
@@ -2,25 +2,44 @@ package com.android.trafficlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout b_1, b_2, b_3;
+    private boolean start_stop = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b_1 = findViewById(R.id.bulb_1);
+        b_2 = findViewById(R.id.bulb_2);
+        b_3 = findViewById(R.id.bulb_3);
 
 
     }
 
     public void onClickStart(View view) {
-        b_1.setBackgroundColor(Color.BLACK);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (start_stop) {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        start_stop = false;
     }
 }
