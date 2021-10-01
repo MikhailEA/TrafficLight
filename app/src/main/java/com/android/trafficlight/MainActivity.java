01fcd5248e48ceceaebf4ce35289c7c41a2188cc
MikhailEA <michev.work@gmail.com>
2021-10-01 18:40:04 +0300

    Замена кнопки start на stop

--- app/src/main/java/com/android/trafficlight/MainActivity.java
+++ app/src/main/java/com/android/trafficlight/MainActivity.java
@@ -5,24 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     private LinearLayout b_1, b_2, b_3;
+    private Button button_1;
     private boolean start_stop = true;
+    private int counter =0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         b_1 = findViewById(R.id.bulb_1);
         b_2 = findViewById(R.id.bulb_2);
         b_3 = findViewById(R.id.bulb_3);
-
-
+        button_1 = findViewById(R.id.button1);
     }
 
     public void onClickStart(View view) {
+        if(!start_stop){
+            button_1.setText("Stop");
+            start_stop = true;
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -35,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+        }else {
+            start_stop = false;
+            button_1.setText("Start");
+        }
     }
 
     @Override
